63fa6cd21aa1f0af7e348a003ac5b9eb9a373737
Sooyoung Yoon <84499458+ddooyn@users.noreply.github.com>
2022-10-31 02:47:55 +0900

    Update ddooyn log

--- packages/core/constants/General/index.ts
+++ packages/core/constants/General/index.ts
@@ -1,7 +1,7 @@
 import { githubUrl } from '../SocialMedia';
 
 // using both app's footer
-export const authorName = '[your name]';
+export const authorName = 'ddooyn';
 
 // Write ever you want :D, default is github
 // 직접 작성해주셔도 좋습니다. 기본적으로 깃허브 주소를 사용합니다.
@@ -11,7 +11,7 @@ export const defaultUrl = githubUrl;
 export * as favicon from './favicon.ico';
 
 // using bot app's header and resume's og:image
-export * as authorImage from './author.png';
+export * as authorImage from './author.jpg';
 
 // using blog og:image
 export * as defaultMetaBackground from './defaultMetaBackground.png';
